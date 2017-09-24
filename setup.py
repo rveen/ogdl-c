6d87d6b39bc149a9608c6fb11172cab443e08374
AlexV <asmodehn@gmail.com>
2017-09-24 18:34:49 +0900

    doing swig from cmake. adding setup.py. fixing naming to match.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for SWIG
+"""
+
+from distutils.core import setup, Extension
+
+#TODO : generate ogdlPYTHON_wrap dynamically on installation (check how pyzmq does it)
+# CMake Ref  :http://www.benjack.io/2017/06/12/python-cpp-tests.html
+
+# but lets KISS
+
+ogdl_module = Extension(
+	'_ogdl',
+    sources=[
+		'src/ogdlPYTHON_wrap.c',
+		'src/graph.c',
+		'src/ogdlbin.c',
+		'src/ogdllog.c',
+		'src/ogdlparser.c',
+		'src/path.c'
+	],
+)
+
+setup (name = 'ogdl',
+       version = '0.1',
+       author      = "AlexV",
+       description = """OGDL extension for python""",
+       ext_modules = [ogdl_module],
+       py_modules = ["ogdl"],
+       )
+
